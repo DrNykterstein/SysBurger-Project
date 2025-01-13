f6a6ea65dcd35ca8778a820416448ac168627ecb
Daniel <46200971+DrNykterstein@users.noreply.github.com>
2025-01-13 00:19:10 -0400

    Inicio de Sesion listo
    Primera vista lista

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,32 @@
 <h1 class="nombre-pagina">SysBurguer</h1>
 <p class="descripcion-pagina">Inicia Sesion</p>
+
+
+<form action="/" method="POST" class="formulario">
+    <div class="campo">
+        <label for="email">Correo Electronico</label>
+        <input 
+            type="email"
+            id="email"
+            placeholder="Tu Correo Electronico"
+            name="email"
+        />
+    </div>
+
+    <div class="campo">
+        <label for="passoword">Contraseña</label>
+        <input 
+            type="password"
+            id="password"
+            placeholder="Escribe tu Contraseña"
+            name="password"
+        />
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesion">
+</form>
+
+<div class="acciones">
+    <a href="/crearCuenta">Crear Cuenta</a>
+    <a href="/recuperar">Olvide mi Contraseña</a>
+</div>
--- views/layout.php
+++ views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App Salón</title>
+    <title>Sistema de Venta de Hamburguesas</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="build/css/app.css">
